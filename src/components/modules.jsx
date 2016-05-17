8d67a24a297c12a4b6f8965039467fd51d540c3b
Vitaliy Illarionov <v.illarionov@office.ngs.ru>
2016-05-17 17:18:26 +0600

    Sticky semantic-react

--- src/components/modules.jsx
+++ src/components/modules.jsx
@@ -26,6 +26,8 @@ export { default as Rating } from './modules/rating/rating';
 export { default as Results } from './modules/search/results';
 export { default as Search } from './modules/search/search';
 
+export { default as Sticky } from './modules/sticky/sticky';
+
 export { default as Tab } from './modules/tab/tab';
 export { default as Tabs } from './modules/tab/tabs';
 export { default as TabTitle } from './modules/tab/tabtitle';
